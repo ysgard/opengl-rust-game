1f0458def0fdc25c29187bfa9a810f34b0990e3e
Ysgard <ysgard@gmail.com>
2018-10-15 12:20:42 -0700

    gl is now a reference counted pointer, and not a static struct

--- lib/gl/src/lib.rs
+++ lib/gl/src/lib.rs
@@ -1,1 +1,31 @@
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+pub use self::bindings::*;
+
+use std::rc::Rc;
+use std::ops::Deref;
+
+#[derive(Clone)]
+pub struct Gl {
+    inner: Rc<bindings::Gl>,
+}
+
+impl Gl {
+    pub fn load_with<F>(loadfn: F) -> Gl
+    where F: FnMut(&'static str) -> *const types::GLvoid
+    {
+        Gl {
+            inner: Rc::new(bindings::Gl::load_with(loadfn))
+        }
+    }
+}
+
+impl Deref for Gl {
+    type Target = bindings::Gl;
+
+    fn deref(&self) -> &bindings::Gl {
+        &self.inner
+    }
+}
